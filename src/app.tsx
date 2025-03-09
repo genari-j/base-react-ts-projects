7ce2681a6682e996e5ade913533cbb263abf3b7d
João Genari <victor.olr@hotmail.com>
2025-03-08 22:28:49 -0300

    feat: adding tanstack to querys

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,17 @@
 import './themes/global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
+import { client } from '~/api/config'
+import { Toaster } from 'react-hot-toast'
 
 import { router } from '~/routes'
 
 export function App() {
 	return (
-		<div>
+		<QueryClientProvider client={client}>
+			<Toaster position="bottom-right" />
 			<RouterProvider router={router} />
-		</div>
+		</QueryClientProvider>
 	)
 }
